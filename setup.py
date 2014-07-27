bfd930a099c2462a9529d5f5c5a342d6281130b8
Philippe Pinard <philippe.pinard@gmail.com>
2014-07-27 13:46:07 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Script information for the file.
+__author__ = "Philippe T. Pinard"
+__email__ = "philippe.pinard@gmail.com"
+__version__ = "0.1"
+__copyright__ = "Copyright (c) 2013 Philippe T. Pinard"
+__license__ = "GPL v3"
+
+# Standard library modules.
+
+# Third party modules.
+from setuptools import setup
+
+# Local modules.
+
+# Globals and constants variables.
+
+setup(name="stratagemtools",
+      version='0.1',
+      url='http://stratagemtools.bitbucket.org',
+      description="Interface to STRATAGem",
+      author="Philippe T. Pinard",
+      author_email="philippe.pinard@gmail.com",
+      license="GPL v3",
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: End Users/Desktop',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Natural Language :: English',
+                   'Programming Language :: Python',
+                   'Operating System :: OS Independent',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Scientific/Engineering :: Physics'],
+
+      packages=['stratagemtools'],
+      zip_safe=True,
+)
+
