b2dd3ccdb56ad94b7267687e2da40c984d78b644
Philippe Pinard <philippe.pinard@gmail.com>
2016-05-03 09:40:34 +0200

    Use versioneer for version

--- setup.py
+++ setup.py
@@ -1,53 +1,23 @@
 #!/usr/bin/env python
 
-# Script information for the file.
-__author__ = "Philippe T. Pinard"
-__email__ = "philippe.pinard@gmail.com"
-__version__ = "0.1"
-__copyright__ = "Copyright (c) 2013 Philippe T. Pinard"
-__license__ = "GPL v3"
-
 # Standard library modules.
-import os
-import re
 import codecs
 
 # Third party modules.
 from setuptools import setup, find_packages
 
 # Local modules.
+import versioneer
 
 # Globals and constants variables.
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-def find_version(*file_paths):
-    """
-    Read the version number from a source file.
-
-    .. note::
-
-       Why read it, and not import?
-       see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
-    """
-    # Open in Latin-1 so that we avoid encoding errors.
-    # Use codecs.open for Python 2 compatibility
-    with codecs.open(os.path.join(BASEDIR, *file_paths), 'r', 'latin1') as f:
-        version_file = f.read()
-
-    # The version line must have the form
-    # __version__ = 'ver'
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
 
 # Get the long description from the relevant file
 with codecs.open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name="stratagemtools",
-      version=find_version('stratagemtools', 'stratagem.py'),
+      version=versioneer.get_version(),
       description="Python interface to SAMx STRATAGem",
       long_description=long_description,
 
@@ -73,5 +43,7 @@ setup(name="stratagemtools",
       install_requires=['pyparsing'],
 
       test_suite='nose.collector',
+
+      cmdclass=versioneer.get_cmdclass(),
 )
 
